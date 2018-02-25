8134beeb49a828d7131d72b5ee656abe389c5594
mjjq <35834600+mjjq@users.noreply.github.com>
2018-02-25 19:00:04 +0000

    Add files via upload

--- source/miscellaneous/stringConversion.cpp
+++ source/miscellaneous/stringConversion.cpp
@@ -9,6 +9,17 @@ std::ostream& operator << ( std::ostream& outs, const sf::Vector2<T> &vec2 )
 }
 
 template <typename T>
+std::ostream& operator << ( std::ostream& outs, const bool &value )
+{
+    std::string temp;
+    if(value)
+        temp = std::string("True");
+    else
+        temp = std::string("False");
+    return outs << temp;
+}
+
+template <typename T>
 std::string to_string( const T &value )
 {
   std::ostringstream ss;
@@ -18,3 +29,4 @@ std::string to_string( const T &value )
 
 template std::string to_string<sf::Vector2i>(const sf::Vector2i &value);
 template std::string to_string<sf::Vector2f>(const sf::Vector2f &value);
+template std::string to_string<bool>(const bool &value);
