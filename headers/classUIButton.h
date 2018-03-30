33d66645dc2dbcb008fcfd0e23ab1df14d3e5d0f
mjjq <marcus_quantrill@hotmail.com>
2018-03-30 09:32:21 +0100

    Migrated UI logic to container class

--- headers/classUIButton.h
+++ headers/classUIButton.h
@@ -13,6 +13,8 @@ protected:
     //sf::RectangleShape buttonRect;
     //std::string buttonText;
     sf::Vector2f currPosition = origPosition;
+    sf::Color clickedColor = {30,30,30,150};
+    sf::Color unclickedColor;
 
     bool displayElement = true;
     //bool fixedToWindow;
@@ -20,6 +22,7 @@ protected:
 public:
     UIButton(std::string text, std::function<void()> clickFunc, sf::Vector2f position, sf::Vector2f bSize, bool fixedToWin, sf::Color color = {80,80,80,150});
     void clickButton();
+    void unclickButton();
     void renderButton(sf::RenderWindow &window, sf::View &GUIView);
     void updateElement(sf::RenderWindow &window, sf::Vector2f parentPosition);
     //void updateElement(sf::RenderWindow &window, sf::View &GUIView, sf::Vector2f parentPosition);
--- headers/classUIContainer.h
+++ headers/classUIContainer.h
@@ -6,17 +6,25 @@ class UIContainer
 {
     std::vector<UIWindow> interfaceWindows;
     std::vector<bool> mouseIntersectionList;
-    //std::vector<bool,int> buttonIntersectList;
+    std::pair<bool,int> currentIntButton;
+    std::pair<bool,int> currentIntWindow;
 
 
 public:
     void addWindow(sf::Vector2f position, float width, float height, bool fixedToWin, bool draggable = false, sf::Color color = {50,50,50,150});
-
     void renderWindows(sf::RenderWindow &window, sf::View &GUIView, sf::View &originalView);
+    UIWindow &getWindow(int windowIndex);
+
     void checkMouseIntersection(sf::RenderWindow &window, sf::View &GUIView, sf::View &originalView);
     std::pair<bool,int> doesMIntExist();
 
-    UIWindow &getWindow(int windowIndex);
+    void clickOnUI(sf::RenderWindow &window);
+    void resetUIClick();
+
+    bool isWindowDraggable();
+    void dragWindow(sf::RenderWindow &window);
+
+
 };
 
 
