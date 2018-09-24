68a2ca392e625c07d3d455195894b5591048ade9
mjjq <marcus_quantrill@hotmail.com>
2018-09-24 09:52:39 +0100

    Added string map creation for keybinds. This generates a list of generated keybinds in string form based on the input keybind json. This map can then be used to display on-screen keybinds via the UI.

--- headers/classKeyBinds.h
+++ headers/classKeyBinds.h
@@ -8,7 +8,9 @@
 #include "../extern/json.hpp"
 #include <fstream>
 
+typedef std::map<std::string, std::string > StringStringMap;
 typedef std::map<std::string, sf::Keyboard::Key> StringKeyMap;
+typedef std::map<sf::Keyboard::Key, std::string> KeyStringMap;
 typedef std::map<std::string, std::function<void()> > StringFuncMap;
 typedef std::map<std::vector<std::string>, std::function<void()> > ArrStringFuncMap;
 typedef std::map<std::vector<sf::Keyboard::Key>, std::function<void()> > KeyFuncMap;
@@ -16,10 +18,17 @@ typedef std::map<std::vector<sf::Keyboard::Key>, std::function<void()> > KeyFunc
 class KeyBinds
 {
     static StringKeyMap keyMap;
+    static KeyStringMap invKeyMap;
 public:
     static KeyFuncMap createMapFromJSON(nlohmann::json &json, StringFuncMap &sfMap);
-    static void loadKeybinds(std::string filePath, std::string sceneType,
-                             StringFuncMap &sfMap, KeyFuncMap &keyMap);
+    static StringStringMap stringMapFromJSON(nlohmann::json &json,
+                                             StringFuncMap &sfMap);
+    static void loadKeybinds(std::string filePath,
+                             std::string sceneType,
+                             StringFuncMap &sfMap,
+                             KeyFuncMap &keyMap,
+                             StringStringMap &stringMap);
+    static std::string to_string(sf::Keyboard::Key &key);
 };
 
 #endif // CLASS_KEYBINDS_H
--- source/events/classKeyBinds.cpp
+++ source/events/classKeyBinds.cpp
@@ -3,111 +3,219 @@
 #define MAP(x) { #x, sf::Keyboard:: x }
 StringKeyMap KeyBinds::keyMap = {
     MAP(A),
-MAP(B),
-MAP(C),
-MAP(D),
-MAP(E),
-MAP(F),
-MAP(G),
-MAP(H),
-MAP(I),
-MAP(J),
-MAP(K),
-MAP(L),
-MAP(M),
-MAP(N),
-MAP(O),
-MAP(P),
-MAP(Q),
-MAP(R),
-MAP(S),
-MAP(T),
-MAP(U),
-MAP(V),
-MAP(W),
-MAP(X),
-MAP(Y),
-MAP(Z),
-MAP(Num0),
-MAP(Num1),
-MAP(Num2),
-MAP(Num3),
-MAP(Num4),
-MAP(Num5),
-MAP(Num6),
-MAP(Num7),
-MAP(Num8),
-MAP(Num9),
-MAP(Escape),
-MAP(LControl),
-MAP(LShift),
-MAP(LAlt),
-MAP(LSystem),
-MAP(RControl),
-MAP(RShift),
-MAP(RAlt),
-MAP(RSystem),
-MAP(Menu),
-MAP(LBracket),
-MAP(RBracket),
-MAP(SemiColon),
-MAP(Comma),
-MAP(Period),
-MAP(Quote),
-MAP(Slash),
-MAP(BackSlash),
-MAP(Tilde),
-MAP(Equal),
-MAP(Dash),
-MAP(Space),
-MAP(Return),
-MAP(BackSpace),
-MAP(Tab),
-MAP(PageUp),
-MAP(PageDown),
-MAP(End),
-MAP(Home),
-MAP(Insert),
-MAP(Delete),
-MAP(Add),
-MAP(Subtract),
-MAP(Multiply),
-MAP(Divide),
-MAP(Left),
-MAP(Right),
-MAP(Up),
-MAP(Down),
-MAP(Numpad0),
-MAP(Numpad1),
-MAP(Numpad2),
-MAP(Numpad3),
-MAP(Numpad4),
-MAP(Numpad5),
-MAP(Numpad6),
-MAP(Numpad7),
-MAP(Numpad8),
-MAP(Numpad9),
-MAP(F1),
-MAP(F2),
-MAP(F3),
-MAP(F4),
-MAP(F5),
-MAP(F6),
-MAP(F7),
-MAP(F8),
-MAP(F9),
-MAP(F10),
-MAP(F11),
-MAP(F12),
-MAP(F13),
-MAP(F14),
-MAP(F15),
-MAP(Pause)
+    MAP(B),
+    MAP(C),
+    MAP(D),
+    MAP(E),
+    MAP(F),
+    MAP(G),
+    MAP(H),
+    MAP(I),
+    MAP(J),
+    MAP(K),
+    MAP(L),
+    MAP(M),
+    MAP(N),
+    MAP(O),
+    MAP(P),
+    MAP(Q),
+    MAP(R),
+    MAP(S),
+    MAP(T),
+    MAP(U),
+    MAP(V),
+    MAP(W),
+    MAP(X),
+    MAP(Y),
+    MAP(Z),
+    MAP(Num0),
+    MAP(Num1),
+    MAP(Num2),
+    MAP(Num3),
+    MAP(Num4),
+    MAP(Num5),
+    MAP(Num6),
+    MAP(Num7),
+    MAP(Num8),
+    MAP(Num9),
+    MAP(Escape),
+    MAP(LControl),
+    MAP(LShift),
+    MAP(LAlt),
+    MAP(LSystem),
+    MAP(RControl),
+    MAP(RShift),
+    MAP(RAlt),
+    MAP(RSystem),
+    MAP(Menu),
+    MAP(LBracket),
+    MAP(RBracket),
+    MAP(SemiColon),
+    MAP(Comma),
+    MAP(Period),
+    MAP(Quote),
+    MAP(Slash),
+    MAP(BackSlash),
+    MAP(Tilde),
+    MAP(Equal),
+    MAP(Dash),
+    MAP(Space),
+    MAP(Return),
+    MAP(BackSpace),
+    MAP(Tab),
+    MAP(PageUp),
+    MAP(PageDown),
+    MAP(End),
+    MAP(Home),
+    MAP(Insert),
+    MAP(Delete),
+    MAP(Add),
+    MAP(Subtract),
+    MAP(Multiply),
+    MAP(Divide),
+    MAP(Left),
+    MAP(Right),
+    MAP(Up),
+    MAP(Down),
+    MAP(Numpad0),
+    MAP(Numpad1),
+    MAP(Numpad2),
+    MAP(Numpad3),
+    MAP(Numpad4),
+    MAP(Numpad5),
+    MAP(Numpad6),
+    MAP(Numpad7),
+    MAP(Numpad8),
+    MAP(Numpad9),
+    MAP(F1),
+    MAP(F2),
+    MAP(F3),
+    MAP(F4),
+    MAP(F5),
+    MAP(F6),
+    MAP(F7),
+    MAP(F8),
+    MAP(F9),
+    MAP(F10),
+    MAP(F11),
+    MAP(F12),
+    MAP(F13),
+    MAP(F14),
+    MAP(F15),
+    MAP(Pause)
 };
 
-KeyFuncMap KeyBinds::createMapFromJSON(nlohmann::json &json, StringFuncMap &sfMap)
+#define INVMAP(x) { sf::Keyboard:: x, #x }
+KeyStringMap KeyBinds::invKeyMap = {
+    INVMAP(A),
+    INVMAP(B),
+    INVMAP(C),
+    INVMAP(D),
+    INVMAP(E),
+    INVMAP(F),
+    INVMAP(G),
+    INVMAP(H),
+    INVMAP(I),
+    INVMAP(J),
+    INVMAP(K),
+    INVMAP(L),
+    INVMAP(M),
+    INVMAP(N),
+    INVMAP(O),
+    INVMAP(P),
+    INVMAP(Q),
+    INVMAP(R),
+    INVMAP(S),
+    INVMAP(T),
+    INVMAP(U),
+    INVMAP(V),
+    INVMAP(W),
+    INVMAP(X),
+    INVMAP(Y),
+    INVMAP(Z),
+    INVMAP(Num0),
+    INVMAP(Num1),
+    INVMAP(Num2),
+    INVMAP(Num3),
+    INVMAP(Num4),
+    INVMAP(Num5),
+    INVMAP(Num6),
+    INVMAP(Num7),
+    INVMAP(Num8),
+    INVMAP(Num9),
+    INVMAP(Escape),
+    INVMAP(LControl),
+    INVMAP(LShift),
+    INVMAP(LAlt),
+    INVMAP(LSystem),
+    INVMAP(RControl),
+    INVMAP(RShift),
+    INVMAP(RAlt),
+    INVMAP(RSystem),
+    INVMAP(Menu),
+    INVMAP(LBracket),
+    INVMAP(RBracket),
+    INVMAP(SemiColon),
+    INVMAP(Comma),
+    INVMAP(Period),
+    INVMAP(Quote),
+    INVMAP(Slash),
+    INVMAP(BackSlash),
+    INVMAP(Tilde),
+    INVMAP(Equal),
+    INVMAP(Dash),
+    INVMAP(Space),
+    INVMAP(Return),
+    INVMAP(BackSpace),
+    INVMAP(Tab),
+    INVMAP(PageUp),
+    INVMAP(PageDown),
+    INVMAP(End),
+    INVMAP(Home),
+    INVMAP(Insert),
+    INVMAP(Delete),
+    INVMAP(Add),
+    INVMAP(Subtract),
+    INVMAP(Multiply),
+    INVMAP(Divide),
+    INVMAP(Left),
+    INVMAP(Right),
+    INVMAP(Up),
+    INVMAP(Down),
+    INVMAP(Numpad0),
+    INVMAP(Numpad1),
+    INVMAP(Numpad2),
+    INVMAP(Numpad3),
+    INVMAP(Numpad4),
+    INVMAP(Numpad5),
+    INVMAP(Numpad6),
+    INVMAP(Numpad7),
+    INVMAP(Numpad8),
+    INVMAP(Numpad9),
+    INVMAP(F1),
+    INVMAP(F2),
+    INVMAP(F3),
+    INVMAP(F4),
+    INVMAP(F5),
+    INVMAP(F6),
+    INVMAP(F7),
+    INVMAP(F8),
+    INVMAP(F9),
+    INVMAP(F10),
+    INVMAP(F11),
+    INVMAP(F12),
+    INVMAP(F13),
+    INVMAP(F14),
+    INVMAP(F15),
+    INVMAP(Pause)
+};
+
+KeyFuncMap KeyBinds::createMapFromJSON(nlohmann::json &json,
+                                       StringFuncMap &sfMap)
 {
-    //newMap contains map{ 'keybrdkey', []{func();} }
+    //create the initial map mapping array of strings (each representing
+    //a keyboard key) to a given function, provided the function sfMap[it.key()]
+    //exists.
     ArrStringFuncMap newMap;
     for(auto it = json.begin(); it != json.end(); ++it)
         if(sfMap.find(it.key()) != sfMap.end())
@@ -118,6 +226,8 @@ KeyFuncMap KeyBinds::createMapFromJSON(nlohmann::json &json, StringFuncMap &sfMa
 
     KeyFuncMap finalMap;
 
+    //Converts the array of string represented keys to sf::Keyboard::keys contained
+    //within newMap.
     for(auto kbArrayIt = newMap.begin(); kbArrayIt != newMap.end(); ++kbArrayIt)
     {
         bool successfulMap = true;
@@ -140,8 +250,52 @@ KeyFuncMap KeyBinds::createMapFromJSON(nlohmann::json &json, StringFuncMap &sfMa
     return finalMap;
 }
 
-void KeyBinds::loadKeybinds(std::string filePath, std::string sceneType,
-                            StringFuncMap &sfMap, KeyFuncMap &keyMap)
+
+StringStringMap KeyBinds::stringMapFromJSON(nlohmann::json &json,
+                                            StringFuncMap &sfMap)
+{
+    //create the initial map mapping array of strings (each representing
+    //a keyboard key) to a given function, provided the function sfMap[it.key()]
+    //exists.
+    StringStringMap newMap;
+    for(auto it = json.begin(); it != json.end(); ++it)
+        if(sfMap.find(it.key()) != sfMap.end())
+        {
+            bool successfulMap  = true;
+            auto tempArray = json[it.key()].get<std::vector<std::string>>();
+            std::string stringBind = "";
+
+            unsigned int i=0;
+            for(auto &kbKey : tempArray)
+            {
+                if(keyMap.find(kbKey) != keyMap.end())
+                {
+                    stringBind += kbKey;
+                    if(i<tempArray.size()-1)
+                        stringBind += " + ";
+                }
+
+                else
+                {
+                    successfulMap = false;
+                    std::cout << kbKey << " doesn't exist in keyMap\n";
+                }
+
+                ++i;
+            }
+            if(successfulMap)
+                newMap.insert( {it.key(), stringBind} );
+        }
+
+    return newMap;
+}
+
+
+void KeyBinds::loadKeybinds(std::string filePath,
+                            std::string sceneType,
+                            StringFuncMap &sfMap,
+                            KeyFuncMap &keyMap,
+                            StringStringMap &stringMap)
 {
     using json = nlohmann::json;
     std::ifstream initInput(filePath);
@@ -151,5 +305,11 @@ void KeyBinds::loadKeybinds(std::string filePath, std::string sceneType,
         json j;
         input >> j;
         keyMap = KeyBinds::createMapFromJSON(j[sceneType], sfMap);
+        stringMap = KeyBinds::stringMapFromJSON(j[sceneType], sfMap);
     }
+}
+
+std::string KeyBinds::to_string(sf::Keyboard::Key &key)
+{
+    return invKeyMap[key];
 }
